5951782702823ecdf68cd6a3e8551e7e8da31705
Ashwini Seshadri <ashwinikseshadri13@gmail.com>
2024-10-22 11:00:07 +0200

    WIP build config for vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: [
+          resolve(__dirname, 'index.html'),
+      ]
+    }
+  },
+  base: '/ashwini-seshadri/',
 })
